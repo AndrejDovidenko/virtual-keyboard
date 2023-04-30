ebf18a8d4e945e6688ffa557824dcdd07fdda1c5
AndrejDovidenko <andrejdovidenko@gmail.com>
2023-04-30 20:12:00 +0300

    feat: add generation of DOM elements

--- page/script.js
+++ page/script.js
@@ -1,3 +1,535 @@
-const a = 0;
-let b = a + 2;
-console.log(b);
+const arrFirst = [
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic big",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+];
+
+const arrSecond = [
+  {
+    class: "button-basic medium",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic medium",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+];
+
+const arrThird = [
+  {
+    class: "button-basic big",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic big",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+];
+
+const arrFourth = [
+  {
+    class: "button-basic big",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+];
+
+const arrFifth = [
+  {
+    class: "button-basic medium",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic space",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic medium",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+  {
+    class: "button-basic",
+    enContentFirst: "~",
+    enContentSecond: "`",
+    ruContentFirst: "~",
+    ruContentSecond: "`",
+  },
+];
+
+function createPage() {
+  const body = document.querySelector(".body");
+  const container = document.createElement("div");
+  const textarea = document.createElement("textarea");
+  const keyboard = document.createElement("div");
+
+  container.className = "container";
+  textarea.className = "textarea";
+  keyboard.className = "keyboard";
+  textarea.rows = "10";
+  textarea.cols = "80";
+  body.appendChild(container);
+  container.appendChild(textarea);
+  container.appendChild(keyboard);
+  keyboard.appendChild(createButton(arrFirst));
+  keyboard.appendChild(createButton(arrSecond));
+  keyboard.appendChild(createButton(arrThird));
+  keyboard.appendChild(createButton(arrFourth));
+  keyboard.appendChild(createButton(arrFifth));
+  return body;
+}
+createPage();
+
+function createButton(arr) {
+  const row = document.createElement("div");
+  row.className = "row";
+  for (let i = 0; i < arr.length; i++) {
+    const button = document.createElement("div");
+    button.className = arr[i].class;
+    const content = `<div class="english">
+      <p>${arr[i].enContentFirst}</p>
+      <p>${arr[i].enContentSecond}</p>
+    </div>
+    <div class="russian">
+      <p>${arr[i].ruContentFirst}</p>
+      <p>${arr[i].ruContentSecond}</p>
+    </div>`;
+    button.innerHTML = content;
+    row.appendChild(button);
+  }
+
+  return row;
+}
